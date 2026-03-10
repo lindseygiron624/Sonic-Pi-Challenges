d4a9f19d7f6898d50ee287029f9bdfe3beba66d3
lindsey <lindsey_giron@student.davincischools.org>
2026-03-10 11:39:46 -0700

    Update ChallengeG.rb

--- ChallengeG.rb
+++ ChallengeG.rb
@@ -19,3 +19,21 @@ live_loop :main_theme do
 end
 
 # Challenge G pt. 2 (Seven Nation Army)
+
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+
+ws_notes = [:e3, :e3, :g3, :e3, :r, :d3, :c3, :b2]
+ws_sleeps = [1.5, 0.5, 0.75, 0.25, 0.5, 0.5, 2, 2]
+
+n = 0
+
+live_loop :white_stripes do
+  8.times do
+    play(ws_notes[n])
+    sleep(ws_sleeps[n])
+    n = n + 1
+  end
+  n = 0
+end
