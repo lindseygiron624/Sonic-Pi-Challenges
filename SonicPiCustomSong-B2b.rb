dd3e5900a8ae8953722a999da4d6589f4535c5a4
lindsey <lindsey_giron@student.davincischools.org>
2026-03-13 10:53:57 -0700

    Update SonicPiCustomSong-B2b.rb

--- SonicPiCustomSong-B2b.rb
+++ SonicPiCustomSong-B2b.rb
@@ -1,21 +1,26 @@
-# B2b2b2b2b
+# B2b
+#my code is not DRY yet!
 
 # 1 sleep = 1 beat = 1
 
 # define samples
-bwow_2 = "C:/Users/lindsey_giron/Downloads/b2b_bwow_main.wav"
 bwow_main = "C:/Users/lindsey_giron/Downloads/b2b_bwow1.wav"
 start = "C:/Users/lindsey_giron/Downloads/b2b_start.wav"
 clap = "C:/Users/lindsey_giron/Downloads/clap.wav"
 
 # define functions
-print sample_duration "C:/Users/lindsey_giron/Downloads/b2b_bwow1.wav"
-# loops !!!!
 
-use_bpm 116
+use_bpm 60
 # starting note
-sample start
-sleep 2
+sample start, beat_stretch: 1
+sleep 1
+
+#live_loop :clap do
+# sample :drum_heavy_kick
+#sleep 1
+#sample clap
+#sleep 1
+#end
 
 # main
 live_loop :bwow do
@@ -31,49 +36,96 @@ live_loop :bwow do
     sample bwow_main
     sleep sample_duration bwow_main
     sample bwow_main
-    sleep 0.2
+    sleep 0.1
   end
   stop
 end
 
-sleep 13.5
-
-play :fs4, sustain: 2
-sleep 0.45
-play :fs4
-sleep 0.25
-play :fs4, sustain: 2
-sleep 0.45
-play :fs4
-sleep 0.25
-play :fs4, sustain: 2
-sleep 0.45
-play :fs4
-sleep 0.25
-play :fs4, sustain: 2
-sleep 0.45
-play :fs4
-sleep 0.3
-play :fs4, sustain: 2
-sleep 0.45
-play :B
-sleep 0.2
-play :Db
-
-# sleep 18
-
-# claps
-# live_loop :kick do
-# sample :drum_heavy_kick
-# sleep 1
-# end
-
-#live_loop :clap do
-# sleep 2
-# sample clap
-#end
+sleep 6.75
 
 # vocals
+use_synth :saw
+# Db = 73 #dsaw #winwood_lead
+amp = 0.1
+
+4.times do
+  play :fs4, amp: amp
+  sleep 0.3
+  amp = amp + 0.05
+  play :fs4, amp: amp
+  sleep 0.2
+  amp = amp + 0.05
+end
+
+
+3.times do
+  play :fs4
+  sleep 0.3
+  play :fs4
+  sleep 0.2
+end
+
+live_loop :back2 do
+  play :fs4
+  sleep 0.15
+  
+  play :B
+  sleep 0.15
+  play 73
+  sleep 0.15
+  play :Bb
+  sleep 0.3
+  
+  5.times do
+    play :fs4
+    sleep 0.15
+  end
+  sleep 0.1
+  
+  play :Ab
+  sleep 0.2
+  play :Bb
+  sleep 0.25
+  play :Db
+  sleep 0.15
+  play :Eb
+  sleep 0.1
+  play :Bb
+  sleep 0.4
+  
+  5.times do
+    play :fs4
+    sleep 0.15
+  end
+  sleep 0.1
+  
+  play :Ab
+  sleep 0.2
+  play :Bb
+  sleep 0.2
+  play :fs4
+  sleep 0.2
+  play :Ab
+  sleep 0.4
+  
+  5.times do
+    play :fs4
+    sleep 0.15
+  end
+  sleep 0.1
+  
+  play :Ab
+  sleep 0.15
+  play :Bb
+  sleep 0.15
+  play :F
+  sleep 0.3
+  play :F
+  sleep 0.15
+  play :Fs4
+  sleep 0.5
+end
+sleep 0.5
 
 # backbackbackback
 
